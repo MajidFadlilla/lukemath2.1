f57433f15474a404ee018fb0a94c5a6f24ef07d2
MajidFadlilla <168385513+MajidFadlilla@users.noreply.github.com>
2024-05-04 22:40:14 +0700

    Update persegi.js

--- luas/persegi.js
+++ luas/persegi.js
@@ -2,4 +2,4 @@ let Panjang = prompt(`masukkan panjang sisi (cm)`)
 const luasPersegi = Panjang * Panjang
 
 alert(`dengan sisi sepanjang ${Panjang}cm,
- maka luas persegi tersebut adalah = ${luasPersegi}cm`)
+ maka luas persegi dengan rumus (L = sisi x sisi = s²) adalah = ${luasPersegi}cm`)
