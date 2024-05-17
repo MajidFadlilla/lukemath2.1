c7f04ef678be10ec656ddf36d591a26311040a7a
MajidFadlilla <168385513+MajidFadlilla@users.noreply.github.com>
2024-05-17 10:04:28 +0700

    Delete keliling directory

--- keliling/kbelahketupat.js
+++
@@ -1,4 +0,0 @@
-let sisi = prompt(`masukkan sisi (cm)`)
-const kelilingBelahKetupat = 4 * sisi
-
-alert(`dengan panjang sisi sepanjang ${sisi}cm, maka keliling belah ketupat tersebut adalah = ${kelilingBelahKetupat}cm`)
--- keliling/kpersegi.js
+++
@@ -1,4 +0,0 @@
-let sisi = prompt(`masukkan sisi (cm)`)
-const kelilingPersegi = 4 * sisi
-
-alert(`dengan panjang sisi sepanjang ${sisi}cm, maka keliling persegi tersebut adalah = ${kelilingPersegi}cm`)
